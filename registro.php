95d06f49a02a43eaa4ab3e290623621a7b36695d
Victor Villalta <viviga04@gmail.com>
2026-05-07 23:39:31 +0200

    WithoutBackground

--- registro.php
+++ registro.php
@@ -1,6 +1,6 @@
 <?php   
 include 'conectar.php';
-include 'enviarEMail.php';
+include 'enviarEmail.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -10,7 +10,6 @@ include 'enviarEMail.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="css/styles.css">
-    <link rel="stylesheet" href="css/login.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script> 
 </head>
@@ -144,7 +143,6 @@ ini_set('display_errors', 1);
             </ul>
         </div> 
         </div>
-
         <a href="index.php" id="menuPrincipial">
             <img src="static/img/logo.png" alt="logo" id="logo">
             <h3 id="textoCabecera">Canary Travel</h3>
