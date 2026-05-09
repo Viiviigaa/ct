29089c5398b2d6fec9be1a860cd0839bef4c18ec
Victor Villalta <viviga04@gmail.com>
2026-05-09 12:51:13 +0200

    Update informacionCuenta

--- informacionCuenta.php
+++ informacionCuenta.php
@@ -71,6 +71,7 @@ try {
     <title>Mi cuenta</title>
     <link rel="stylesheet" href="css/styles.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <style>
     .lenguajes {
