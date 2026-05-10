c1d35f2f6c4433b51b29008313abd3817e6cb2ae
Victor Villalta <viviga04@gmail.com>
2026-05-10 19:28:42 +0200

    Update panelAdministrador.php

--- panelAdministrador.php
+++ panelAdministrador.php
@@ -707,6 +707,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
                     <button type="submit" name="accion_update_user" class="btn btn-primary">Guardar Cambios</button>
                 </div>
+            </form>
             </div>
         </div>
     </div>
